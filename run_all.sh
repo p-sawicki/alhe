8548682e1a669674410d0d84dc8e26ee63f4b86d
Piotr Sawicki <sawickipiotr@outlook.com>
2021-05-22 21:47:08 +0200

    add .csv to log files in run_all.sh

--- run_all.sh
+++ run_all.sh
@@ -1,4 +1,4 @@
-./compare.py --log output/log_single_1
-./compare.py --log output/log_multi_1 --multi-mode
-./compare.py --log output/log_single_10 --modularity 10
-./compare.py --log output/log_multi_10 --multi-mode --modularity 10
+./compare.py --log output/log_single_1.csv
+./compare.py --log output/log_multi_1.csv --multi-mode
+./compare.py --log output/log_single_10.csv --modularity 10
+./compare.py --log output/log_multi_10.csv --multi-mode --modularity 10
